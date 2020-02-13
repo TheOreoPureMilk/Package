e79a3752f040ab3ade0aff87f3ce016549d7b7d1
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-13 17:36:36 +0800

    package

---
+++ build/precache-manifest.34f7d1c3028f3863554bab19cfc18a2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857e0e71854ae058c880cd5fe4fe210c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ba022d52da7388baf2",
+    "url": "/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "67ebae080b86bb1225bd",
+    "url": "/static/css/main.c0fbced9.chunk.css"
+  },
+  {
+    "revision": "51ba022d52da7388baf2",
+    "url": "/static/js/2.b7eaaebe.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.b7eaaebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ebae080b86bb1225bd",
+    "url": "/static/js/main.3bef27d5.chunk.js"
+  },
+  {
+    "revision": "032a2bfbf292a2a554d8",
+    "url": "/static/js/runtime-main.cfe0f948.js"
+  }
+]);
