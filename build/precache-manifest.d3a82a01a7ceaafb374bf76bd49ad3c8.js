fec977cc6351fc85a43f199220e1c81e7cb81cb4
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-14 19:02:28 +0800

    详情页面路由问题

---
+++ build/precache-manifest.d3a82a01a7ceaafb374bf76bd49ad3c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f375418f90a01a4d29ff7339c9034a28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5894c833b87f6400b19f",
+    "url": "./static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "15b25a868af7aaf1fc43",
+    "url": "./static/css/main.7849cd38.chunk.css"
+  },
+  {
+    "revision": "5894c833b87f6400b19f",
+    "url": "./static/js/2.34f56478.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "./static/js/2.34f56478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b25a868af7aaf1fc43",
+    "url": "./static/js/main.016455a3.chunk.js"
+  },
+  {
+    "revision": "3d0013275c2586a3709f",
+    "url": "./static/js/runtime-main.071c7897.js"
+  }
+]);
